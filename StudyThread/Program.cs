4e0dc6da70b0bdb76bc6b103979a7b827fd6c7f6
kristovanoha <kristovanoha@centrum.cz>
2015-03-20 07:07:23 +0100

    prvni thread

---
+++ StudyThread/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            object dosts = new object();
+
+            Dost dost = new Dost();
+
+
+            Console.WriteLine("konec");
+            Console.ReadKey();
+        }
+
+        public class Dost
+        {
+            public int neco { get; set; }
+        }
+
+        private void cc()
+        {
+            string s = "sdf";
+        }
+    }
+}
